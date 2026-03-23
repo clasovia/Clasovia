e12d0361319d6d18437869bcc052b2516ccf8644
SANTOSH <developersantosh18@gmail.com>
2026-03-23 19:50:42 +0530

    after client or server setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const Razorpay = require("razorpay");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
--- server/routes/registrationRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { registerForWebinar } from '../controllers/registrationController.js'
-
-const router = express.Router()
-
-router.post('/', registerForWebinar)
-
-export default router
